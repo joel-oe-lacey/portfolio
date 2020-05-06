e516eaa6b825f4c96c987a24503cbeece2cc0f10
Joel Lacey <joellaceybusiness@gmail.com>
2020-05-06 17:31:44 -0600

    Outline Nav structure

---
+++ src/components/Nav.vue
@@ -0,0 +1,28 @@
+<template>
+  <section id="nav">
+      <a href="">About</a>
+      <a href="">Porfolio</a>
+      <a href="">Misc</a>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  components: {
+  }
+}
+</script>
+
+<style>
+#nav {
+    float: right;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    height: 20%;
+    width: 60%;
+    border: 1px solid black;
+}
+</style>
