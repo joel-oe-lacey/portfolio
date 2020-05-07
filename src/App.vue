a8dbfe5f3276f641dc911131e4b67fc7cbc31d04
Joel Lacey <joellaceybusiness@gmail.com>
2020-05-06 21:28:23 -0600

    Import component wireframes into App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="scroll-wrapper">
+      <Nav />
+      <Landing />
+      <InfoBlock />
+      <InfoBlock />
+      <InfoBlock />
+    </div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Landing from './components/Landing.vue';
+import Nav from './components/Nav.vue';
+import InfoBlock from './components/InfoBlock.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Landing,
+    Nav,
+    InfoBlock
   }
 }
 </script>
 
 <style>
 #app {
+  /* position: relative; */
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  flex: 0 0 auto;
+  height: 100%;
+  width: 100%;
+}
+#scroll-wrapper {
+  /* position: absolute; */
+  display: flex;
+  flex-flow: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  overflow: auto;
 }
 </style>
