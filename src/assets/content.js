0b33b168b9a42d3d9337c9d62c784449a9d1081c
Joel Lacey <joellaceybusiness@gmail.com>
2020-05-12 12:15:28 -0600

    Create data hold

---
+++ src/assets/content.js
@@ -0,0 +1,17 @@
+export const about = {
+    title: 'About Me',
+    desc: 'A front-end developer utilizing strong interpersonal team skills, an inquisitive approach to research, and a love for deconstruction to create humane and sustainable technological solutions.'
+}
+
+export const skills = {
+    title: 'Skills',
+    items: ['React',
+    'React Native',
+    'Vue',
+    'TypeScript',
+    'Node.JS',
+    'JavaScript',
+    'jQuery',
+    'HTML / CSS',
+    'SCSS']
+}
