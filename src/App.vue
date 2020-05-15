cce6ccb27e29dd955344378b4721c2ec34cddaa6
Joel Lacey <joellaceybusiness@gmail.com>
2020-05-14 20:48:35 -0600

    Modify App to utilize landing page abstraction

--- src/App.vue
+++ src/App.vue
@@ -2,47 +2,21 @@
   <div id="app">
     <div id="scroll-wrapper">
       <Nav />
-      <Landing />
-      <InfoBlock v-bind="about"/>
-      <InfoBlock v-bind="skills"/>
-      <h1>Experience</h1>
-      <ExperienceBlock 
-        v-for="item in resume.items"
-        v-bind:key="item.title"
-        v-bind="item"/>
-      <h1>Education</h1>
-      <ExperienceBlock 
-        v-for="item in education.items"
-        v-bind:key="item.title"
-        v-bind="item"/>
+      <About />
     </div>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Landing from './components/Landing.vue';
+import About from './components/About.vue';
 import Nav from './components/Nav.vue';
-import InfoBlock from './components/InfoBlock.vue';
-import ExperienceBlock from './components/ExperienceBlock.vue';
-import { about, skills, resume, education } from './assets/content.js';
 
 export default {
   name: 'App',
   components: {
-    Landing,
-    Nav,
-    InfoBlock,
-    ExperienceBlock
-  },
-  data: function() {
-        return {
-          about,
-          skills,
-          resume,
-          education
-        }
-    }
+    About,
+    Nav
+  }
 }
 </script>
 
@@ -61,7 +35,7 @@ export default {
   /* position: absolute; */
   display: flex;
   flex-flow: column;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
   height: 100%;
   width: 100%;
