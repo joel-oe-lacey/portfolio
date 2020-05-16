47ef562feac8c2e5c38ba066a385b078fd7d135d
Joel Lacey <joellaceybusiness@gmail.com>
2020-05-15 22:02:45 -0600

    Add portfolio and portfolio block wireframe

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="inner-wrapper">
+      <h1>Portfolio</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Portfolio',
+  components: {
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  flex: 0 0 auto;
+  height: 300vh;
+  width: 100%;
+  overflow-y: scroll;
+}
+#inner-wrapper {
+  display: flex;
+  flex-flow: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 90%;
+  width: 100%;
+  text-align: left;
+}
+</style>
---
+++ src/components/PortfolioBlock.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <section class="resumeItem">
+      <section class="leftHeader">
+          <h2>{{company}}</h2>
+          <h2>{{title}}</h2>
+      </section>
+      <section class="rightHeader">
+          <h2>{{location}}</h2>
+          <h2>{{`${sdate} - ${edate}`}}</h2>
+      </section>
+      <ul class="body">
+          <li
+          v-for="point in points" :key="point">{{point}}
+          </li>
+      </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'InfoBlock',
+  components: {
+  },
+  props: ['company', 'sdate', 'edate', 'location', 'title', 'points']
+}
+</script>
+
+<style>
+.leftHeader {
+  height: 40%;
+  width: 70%;
+}
+.rightHeader {
+  height: 40%;
+  width: 30%;
+}
+.body {
+  height: 60%;
+  width: 100%;
+  margin-top: 2%;  
+}
+.resumeItem {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    align-items: flex-start;
+    padding: 3%;
+    height: 40%;
+    width: 80%;
+    border: 1px solid black;
+}
+</style>
