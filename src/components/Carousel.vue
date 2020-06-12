894a074b04bfd63304c11315b0c8f49afb128e86
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-12 11:11:09 -0600

    Add Carousel component for journey display

---
+++ src/components/Carousel.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="carousel-outer">
+    <section class="carousel">
+      <slot/>
+    </section>
+    <button @click="changeSlide(-1)">Prev Slide</button>
+    <button @click="changeSlide(1)">Next Slide</button>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    changeSlide(delta) {
+      const carousel = this.$el.querySelector(".carousel");
+      const width = carousel.offsetWidth;
+      carousel.scrollTo(carousel.scrollLeft + width * delta, 0);
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.carousel-outer {
+  height: 40%;
+  width: 100%;
+}
+.carousel {
+  display: flex;
+  overflow: scroll;
+  scroll-behavior: smooth;
+  scroll-snap-type: x mandatory;
+  height: 100%;
+  width: 100%;
+}
+
+.carousel > * {
+  flex: 1 0 100%;
+  scroll-snap-align: start;
+}
+</style>
