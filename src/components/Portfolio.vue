7bbd3181ee40083df70f50b211629b4bbac9c20c
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-12 15:54:00 -0600

    Add parallax display to Portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,44 +1,63 @@
 <template>
     <div class="port-wrapper">
-      <Carousel width="100%" height="50%">
+      <div class="parallax-container">
+        <parallax>
+          <img src="../assets/street.jpg" class="background">
+        </parallax>
+      </div>
+      <Landing v-bind="portLanding" />
+      <h2 class='port-header'>Projects</h2>
+      <Carousel width="90%" height="45%">
         <PortfolioBlock 
         v-for="(item, index) in projects"
         v-bind:key="index"
         v-bind="item"/>
       </Carousel>
+      <h2 class='port-header'>About this site</h2>
     </div>
 </template>
 
 <script>
+import Parallax from "vue-parallaxy";
 import PortfolioBlock from './PortfolioBlock.vue';
-import { projects } from '../assets/content.js';
+import { skills, projects, portLanding } from '../assets/content.js';
 import Carousel from './Carousel.vue';
+import Landing from './Landing.vue';
 
 export default {
   name: 'Portfolio',
   components: {
     PortfolioBlock,
-    Carousel
+    Carousel,
+    Parallax,
+    Landing
   },
   data: function() {
       return {
-        projects
+        skills,
+        projects,
+        portLanding
       }
   }
 }
 </script>
 
 <style>
-.app {
-  height: 100vh;
-  width: 100%;
-}
 .port-wrapper {
   display: flex;
   flex-flow: column;
   align-items: center;
-  margin-top: 10%;
   height: 100%;
   width: 100%;
+}
+.port-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  height: 5%;
+  width: 90%;
+  color: #C4D3E7;
+  font-size: 2rem;
 }
 </style>
