c819048b4b3a68c5c740fc16f667720476b8f970
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-12 14:44:59 -0600

    Refactor PortfolioBlock structure

--- src/components/PortfolioBlock.vue
+++ src/components/PortfolioBlock.vue
@@ -1,18 +1,27 @@
 
 <template>
-  <section class="portfolioItem">
-    <section class="portfolioBody">
-      <h1 class="projHeader">{{title}}</h1>
-      <section class="about">
-          <h2>About</h2>
-          <p>{{desc}}</p>
+  <section class="portfolio-item">
+    <section class="portfolio-body">
+      <h1 class="proj-header">{{title}}</h1>
+      <section class='port-desc'>
+        <p v-for="(paragraph, index) in desc" :key="index">
+          {{paragraph}}
+        </p>
       </section>
-      <ul class="stack">
-          <li
-          v-for="item in stack" :key="item">{{item}}
-          </li>
-      </ul>
-      <img class="projectImg" src="../assets/american_portrait.png" />
+      <section class="stack">
+        <p
+        v-for="item in stack" :key="item">{{item}}
+        </p>
+      </section>
+      <section class="project-img-cont">
+        <img class="project-img" src="../assets/american_portrait.png" />  
+      </section>
+      <section class="btn-cont-github">
+        <a :href="github" class="btn">Github</a>
+      </section>
+      <section class="btn-cont-live">
+        <a :href="live" class="btn">Live Site</a>
+      </section>
     </section>
   </section>
 </template>
@@ -22,20 +31,21 @@ export default {
   name: 'PortfolioBlock',
   components: {
   },
-  props: ['title', 'link', 'desc', 'stack', 'img']
+  props: ['title', 'github', 'live', 'desc', 'stack', 'img']
 }
 </script>
 
 <style>
-.portfolioBody {
+.portfolio-body {
   display: grid;
   grid-template-columns: 50% 50%;
-  grid-template-rows: 10% 45% 45%;
+  grid-template-rows: 10% 70% 5% 15%;
   grid-gap: 2%;
   grid-template-areas: 
   "header header"
   "about img"
-  "stack img";
+  "stack stack"
+  "github live";
   height: 96%;
   width: 98%;
   padding: 3%;
@@ -45,12 +55,12 @@ export default {
   border-top-left-radius: 4% 9%;
   background-color: #9590A8;
 }
-.portfolioItem {
+.portfolio-item {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 40%;
+  height: 90%;
   width: 90%;
   margin: 3%;
   /* border-radius: 1%; */
@@ -59,26 +69,81 @@ export default {
   border-top-right-radius: 5% 10%;
   border-bottom-left-radius: 5% 10%;
   border-top-left-radius: 5% 10%;
+  transition: 800ms;
 }
-.portfolioItem:hover {
+.portfolio-item:hover {
   box-shadow: 0.1rem 0.3rem 0.6rem #9590A8;
   transition: 300ms;
 }
-.projHeader {
+.proj-header {
   font-size: 2em;
   grid-area: header;
   color: #82455D;
 }
-.about {
+.port-desc {
   grid-area: about;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 }
 .stack {
   grid-area: stack;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  font-weight: bold;
+  font-size: 1.3rem;
+  color: #82455D;
 }
-.projectImg {
+.project-img {
+  height: 50%;
+  width: 100%;
+  object-fit: scale-down;
+}
+.project-img-cont {
   height: 100%;
   width: 100%;
   grid-area: img;
-  object-fit: scale-down;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+.btn-cont-github {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  text-decoration: none;
+  grid-area: github;
+}
+.btn-cont-live {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  grid-area: live;
+}
+.btn {
+  height: 60%;
+  width: 40%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  background-color: #C4D3E7;
+  color: #82455D;
+  font-size: 1.8rem;
+  font-weight: bold;
+  border: 2px solid #82455D;
+  border-bottom-right-radius: 5% 10%;
+  border-top-right-radius: 5% 10%;
+  border-bottom-left-radius: 5% 10%;
+  border-top-left-radius: 5% 10%;
 }
 </style>
